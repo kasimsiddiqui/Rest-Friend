2b2829a16677c1c0acee37079976de534bd0b532
David Sheffield <david.sheffield21@gmail.com>
2015-09-13 16:50:39 -0700

    updating utility.js and utility_test.js

---
+++ test/utility_test.js
@@ -0,0 +1,58 @@
+var chai = require('chai');
+var chaihttp = require('chai-http');
+var expect = chai.expect;
+chai.use(chaihttp);
+var server = require(__dirname + '/../lib/server');
+var Router = require(__dirname + '/../lib/router');
+var utility = require(__dirname + '/../lib/utility');
+
+
+describe('the Utility Router', function() {
+  it('should return status 200', function(done) {
+    chai.request('localhost:3000')
+      .end(function (err, res) {
+        expect(err).to.be.equal(null);
+        expect(res.status).to.be.equal(200);
+        done();
+      });
+  });
+  it('should return status 404', function(done) {
+    chai.request('localhost:3000')
+      .end(function (err, res) {
+        expect(res.status).to.be.equal(404);
+        done();
+      });
+  });
+  it('should complete a GET request', function(done) {
+    chai.request('localhost:3000')
+      .end(function (err, res) {
+        expect(err).to.be.equal(null);
+        expect(res.status).to.be.equal(200);
+        done();
+      });
+  });
+  it('should complete a POST request', function(done) {
+      chai.request('localhost:3000')
+        .end(function (err, res) {
+          expect(err).to.be.equal(null);
+          expect(res.status).to.be.equal(200);
+          done();
+        });
+    });
+  it('should complete a PUT request', function(done) {
+    chai.request('localhost:3000')
+      .end(function (err, res) {
+        expect(err).to.be.equal(null);
+        expect(res.status).to.be.equal(200);
+        done();
+      });
+  });
+  it('should complete a PATCH request', function(done) {
+    chai.request('localhost:3000')
+      .end(function (err, res) {
+        expect(err).to.be.equal(null);
+        expect(res.status).to.be.equal(200);
+        done();
+      });
+  });
+});
