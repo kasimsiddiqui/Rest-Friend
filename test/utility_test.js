764af2067218222f98d83513dce14d139a141165
David Sheffield <david.sheffield21@gmail.com>
2015-09-13 17:15:38 -0700

    updating utility_test.js

--- test/utility_test.js
+++ test/utility_test.js
@@ -10,6 +10,7 @@ var utility = require(__dirname + '/../lib/utility');
 describe('the Utility Router', function() {
   it('should return status 200', function(done) {
     chai.request('localhost:3000')
+      .status200('./')
       .end(function (err, res) {
         expect(err).to.be.equal(null);
         expect(res.status).to.be.equal(200);
@@ -18,6 +19,7 @@ describe('the Utility Router', function() {
   });
   it('should return status 404', function(done) {
     chai.request('localhost:3000')
+      .status404('./other')
       .end(function (err, res) {
         expect(res.status).to.be.equal(404);
         done();
@@ -25,6 +27,7 @@ describe('the Utility Router', function() {
   });
   it('should complete a GET request', function(done) {
     chai.request('localhost:3000')
+      .get('./')
       .end(function (err, res) {
         expect(err).to.be.equal(null);
         expect(res.status).to.be.equal(200);
@@ -33,6 +36,7 @@ describe('the Utility Router', function() {
   });
   it('should complete a POST request', function(done) {
       chai.request('localhost:3000')
+        .post('./data')
         .end(function (err, res) {
           expect(err).to.be.equal(null);
           expect(res.status).to.be.equal(200);
@@ -41,6 +45,7 @@ describe('the Utility Router', function() {
     });
   it('should complete a PUT request', function(done) {
     chai.request('localhost:3000')
+      .put('./data')
       .end(function (err, res) {
         expect(err).to.be.equal(null);
         expect(res.status).to.be.equal(200);
@@ -49,6 +54,7 @@ describe('the Utility Router', function() {
   });
   it('should complete a PATCH request', function(done) {
     chai.request('localhost:3000')
+      .patch('./data')
       .end(function (err, res) {
         expect(err).to.be.equal(null);
         expect(res.status).to.be.equal(200);
