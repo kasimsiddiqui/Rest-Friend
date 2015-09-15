da6dafaa23929e32615cee92e5684697eb8888f1
David Sheffield <david.sheffield21@gmail.com>
2015-09-15 15:09:01 -0700

    updating functionality of our framework to -> MVP

--- test/server_test.js
+++ test/server_test.js
@@ -2,9 +2,20 @@
 
 var http = require('http');
 var port = 3000;
+var Router = require(__dirname + '/../lib/router');
+var restFriend = require(__dirname + '/../lib/routes');
 
-var server = http.createServer(function(req, res) {
+var router = Router();
+
+router.get('/', function(req, res){
+    restFriend(res).status200('hello world');
 });
+
+router.get('/dontgohere', function(req, res){
+    restFriend(res).error404('error404 page not found');
+});
+
+var server = http.createServer(router.route);
 
 server.listen(port, function() {
   console.log('Server is running on port ' + port);
